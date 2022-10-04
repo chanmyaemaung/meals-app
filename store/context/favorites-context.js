ab9329444b473a1459f218d7377dd703d40bba71
Chen Lay <maungmaungyoker.mmy@gmail.com>
2022-10-04 22:55:06 +0630

    featured: Successfully built with reactContext State :memo:

---
+++ store/context/favorites-context.js
@@ -0,0 +1,33 @@
+import { createContext, useState } from 'react'
+
+export const FavoritesContext = createContext({
+	ids: [],
+	addFavorite: (id) => {},
+	removeFavorite: (id) => {},
+})
+
+export default function FavoritesContextProvider({ children }) {
+	const [favoriteMealIds, setFavoriteMealIds] = useState([])
+
+	function addFavorite(id) {
+		setFavoriteMealIds((currentFavIds) => [...currentFavIds, id])
+	}
+
+	function removeFavorite(id) {
+		setFavoriteMealIds((currentFavIds) =>
+			currentFavIds.filter((mealId) => mealId !== id)
+		)
+	}
+
+	const value = {
+		ids: favoriteMealIds,
+		addFavorite,
+		removeFavorite,
+	}
+
+	return (
+		<FavoritesContext.Provider value={value}>
+			{children}
+		</FavoritesContext.Provider>
+	)
+}
